4acefb7eafeb21bcf19dcaf4935ba49077f20b8a
duzao <eduardoneriscarneiro@gmail.com>
2023-05-05 13:55:13 -0300

    nao consigo achar a menor nem a maior palavra

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let tec = ['java', 'javascript', 'python'];
+
+let quantidadeDeLetras = tec;
+
+for (let index = 0; index > tec.length; index += 1);{
+  if (tec.length < quantidadeDeLetras.length) {
+    quantidadeDeLetras = tec[index];
+
+     }
+}
+console.log(quantidadeDeLetras);
